d8220a2255e8c8637f6714bebcc3b5e7a92e35a2
JayShang <yijieshang@gmail.com>
2018-05-14 10:01:15 +0800

    创建自己的框架，通过cocospod来用，oc版本

---
+++ 我的项目/我的框架/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  我的项目
+//
+//  Created by caihongguang on 2018/5/14.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name = "小芳"
+    
+}
--- 我的项目/我的项目/ViewController.swift
+++ 我的项目/我的项目/ViewController.swift
@@ -4,10 +4,10 @@
 //
 //  Created by caihongguang on 2018/5/14.
 //  Copyright © 2018年 SYJ. All rights reserved.
-//
+//http://www.iqiyi.com/v_19rrb3mz8o.html oc版本
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
